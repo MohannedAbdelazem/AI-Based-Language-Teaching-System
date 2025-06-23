563ba054b3d4f9d632eb8d496eac0a25c0ae429b
Mohamed Omar <m01010691810@gmail.com>
2025-06-23 15:59:14 +0300

    Initial commit on new-branch

--- obj/Debug/net8.0/AI-based-Language-Teaching.AssemblyInfo.cs
+++ obj/Debug/net8.0/AI-based-Language-Teaching.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI-based-Language-Teaching")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3873bab325eaaaa76fbe63c0b1c16365297e792")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI-based-Language-Teaching")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI-based-Language-Teaching")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
