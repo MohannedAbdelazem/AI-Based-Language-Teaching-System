1cc1d170a390ba723a9c736a1a9d9c44fac83761
you22ef <102329215+you22ef@users.noreply.github.com>
2025-06-26 01:50:18 +0300

    scroll behaviour

--- FluentFlow/src/app/layout/pages/profile/profile.component.ts
+++ FluentFlow/src/app/layout/pages/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { TopicsService } from '../../../services/Topics/topics.service';
 import { Topic } from '../../../interfaces/Topics/topics';
 import { ProgressComponent } from '../progress/progress.component';
@@ -14,6 +14,13 @@ import { NavbarComponent } from '../../additions/navbar/navbar.component';
 })
 export class ProfileComponent 
 {
+
+  title: string = 'Past Simple';
+  titleNumber: number = 1;
+
+  @ViewChildren('lineElement') lineElements!: QueryList<ElementRef>;
+  @ViewChild('topicText') topicText!: ElementRef;
+
   constructor(private __topicService: TopicsService) 
   {
     
@@ -30,5 +37,33 @@ export class ProfileComponent
   return Array.from({ length: n }, (_, i) => i + 1);
   }
 
+  @HostListener('window:scroll', [])onWindowScroll() 
+  {
+
+    this.checkCollision();
+  }
+
+  checkCollision() 
+  {
+    const lines = this.lineElements.toArray();
+    const topicRect = this.topicText.nativeElement.getBoundingClientRect();
+
+    for (let i = 0; i < lines.length; i++) 
+      {
+        const lineRect = lines[i].nativeElement.getBoundingClientRect();
+
+        const isTouching =
+          lineRect.bottom >= topicRect.top &&
+          lineRect.top <= topicRect.bottom &&
+          lineRect.right >= topicRect.left &&
+          lineRect.left <= topicRect.right;
+
+        if (isTouching) 
+        {
+          this.title = lines[i].nativeElement.innerText;
+          this.titleNumber = i + 1;
+        }
+    }
+  }
 
 }
